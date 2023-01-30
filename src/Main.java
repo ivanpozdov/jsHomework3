25e1b7f613c4dbe5f792d32a520e568dc5168861
Ivan Pozdov <Ivan.pozdov58@gmail.com>
2023-01-30 13:46:44 +0300

    Reformate code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+        int price = 10000;
         int miles = service.calculate(price);
         System.out.println(miles);
         BonusMilesService calculate = new BonusMilesService();
